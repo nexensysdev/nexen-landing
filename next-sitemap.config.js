fbf225484119800fc4b44b1f05ba62e59eade8fc
Claudio M. Di Toro <cditoro@gmail.com>
2026-05-19 21:35:46 -0300

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,45 +1,71 @@
+const siteUrl = 'https://nexensys.ar'
+
+function getAlternatePaths(path) {
+  const projectEsMatch = path.match(/^\/proyectos\/([^/]+)$/)
+  const projectEnMatch = path.match(/^\/en\/projects\/([^/]+)$/)
+
+  if (path === '/' || path === '/en') {
+    return {
+      spanishPath: '/',
+      englishPath: '/en',
+    }
+  }
+
+  if (projectEsMatch) {
+    return {
+      spanishPath: path,
+      englishPath: `/en/projects/${projectEsMatch[1]}`,
+    }
+  }
+
+  if (projectEnMatch) {
+    return {
+      spanishPath: `/proyectos/${projectEnMatch[1]}`,
+      englishPath: path,
+    }
+  }
+
+  return {
+    spanishPath: path,
+    englishPath: path,
+  }
+}
+
+function alternateRefs(spanishPath, englishPath) {
+  return [
+    {
+      href: `${siteUrl}${spanishPath}`,
+      hreflang: 'es-AR',
+      hrefIsAbsolute: true,
+    },
+    {
+      href: `${siteUrl}${englishPath}`,
+      hreflang: 'en',
+      hrefIsAbsolute: true,
+    },
+    {
+      href: `${siteUrl}${spanishPath}`,
+      hreflang: 'x-default',
+      hrefIsAbsolute: true,
+    },
+  ]
+}
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://nexensys.ar',
+  siteUrl,
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
   transform: async (config, path) => {
-    const projectEsMatch = path.match(/^\/proyectos\/([^/]+)$/)
-    const projectEnMatch = path.match(/^\/en\/projects\/([^/]+)$/)
-
-    let spanishPath = '/'
-    let englishPath = '/en'
-
-    if (projectEsMatch) {
-      spanishPath = path
-      englishPath = `/en/projects/${projectEsMatch[1]}`
-    }
-
-    if (projectEnMatch) {
-      spanishPath = `/proyectos/${projectEnMatch[1]}`
-      englishPath = path
-    }
+    const { spanishPath, englishPath } = getAlternatePaths(path)
 
     return {
       loc: path,
       changefreq: config.changefreq,
       priority: path === '/' || path === '/en' ? 0.9 : config.priority,
       lastmod: new Date().toISOString(),
-      alternateRefs: [
-        {
-          href: `${config.siteUrl}${spanishPath}`,
-          hreflang: 'es-AR',
-        },
-        {
-          href: `${config.siteUrl}${englishPath}`,
-          hreflang: 'en',
-        },
-        {
-          href: `${config.siteUrl}${spanishPath}`,
-          hreflang: 'x-default',
-        },
-      ],
+      alternateRefs: alternateRefs(spanishPath, englishPath),
     }
   },
 }
